7f80c1c621381f1f264248f08c2f76627809c65a
KyubinHwang <choco1drink@gmail.com>
2021-12-20 16:09:48 +0900

    추가

--- BOJ_15686/main.cpp
+++ BOJ_15686/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  BOJ_15686
-//
-//  Created by 황규빈 on 2021/12/20.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
