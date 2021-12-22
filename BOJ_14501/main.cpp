161429f1affda33b515c514a1d5d0a265a3f9a34
KyubinHwang <choco1drink@gmail.com>
2021-12-22 16:04:58 +0900

    추가

--- BOJ_14501/main.cpp
+++ BOJ_14501/main.cpp
@@ -18,9 +18,6 @@ int main(int argc, const char * argv[]) {
         P[i] = tmp2;
     }
     
-    dp[N + 1] = 0;
-    dp[N] = 0;
-    
     for(int i = N; i >= 1; i--){
         if(i + T[i] - 1 > N){
             dp[i] = dp[i + 1];
