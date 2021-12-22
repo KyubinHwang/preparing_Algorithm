997b9ff4f79a2261e51e783e25114598f5c85711
KyubinHwang <choco1drink@gmail.com>
2021-12-22 14:18:15 +0900

    Initial Commit

---
+++ BOJ_14501/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BOJ_14501
+//
+//  Created by 황규빈 on 2021/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
