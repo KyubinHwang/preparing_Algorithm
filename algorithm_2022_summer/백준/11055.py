be9c4be936fac8820e8d1b52ac02ea34eb8f9531
KyubinHwang <choco1drink@gmail.com>
2022-07-11 17:51:17 +0900

    백준 11055, 1699 업로드

---
+++ algorithm_2022_summer/백준/11055.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+lst = list(map(int, input().split()))
+dp = [x for x in lst]
+
+for i in range (N):
+    for j in range (i):
+        if lst[i] > lst[j]:
+            dp[i] = max(dp[i], dp[j] + lst[i])
+
+print(max(dp))
